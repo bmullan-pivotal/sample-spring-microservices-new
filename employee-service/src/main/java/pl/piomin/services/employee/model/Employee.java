6e5cf92154f7cea33eabfae85437217bf403eda5
bmullan-pivotal <bmullan@pivotal.io>
2021-10-11 18:38:34 -0400

    updated _id

--- employee-service/src/main/java/pl/piomin/services/employee/model/Employee.java
+++ employee-service/src/main/java/pl/piomin/services/employee/model/Employee.java
@@ -8,6 +8,7 @@ public class Employee {
 
 	@Id
 	private String id;
+	private String _id; 
 	private Long organizationId;
 	private Long departmentId;
 	private String name;
@@ -25,6 +26,15 @@ public class Employee {
 		this.age = age;
 		this.position = position;
 	}
+
+	public String get_Id() {
+		return _id;
+	}
+
+	public void set_Id(String _id) {
+		this._id = _id;
+	}
+
 
 	public String getId() {
 		return id;
